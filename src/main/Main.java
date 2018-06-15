2d1454674db2b27acf0cf8f7b59d5cbcfabca78f
Unknown <tomas.slama.131@gmail.com>
2018-06-15 14:31:39 -0400

    add custom chassis visualisation
    You can now, using left mouse button, define the size of the chassis of the robot in the simulation.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,12 @@ package main;
 import processing.core.*;
 
 public class Main extends PApplet {
-    // Our input object
+    // Input object
     Input input;
+
+    // Variables of the swerve drivebase
+    int W = 0;
+    int H = 0;
 
     static public void main(String[] passedArgs) {
         PApplet.main(Main.class.getName());
@@ -12,6 +16,7 @@ public class Main extends PApplet {
 
     @Override
     public void settings() {
+        size(720, 480);
     }
 
     @Override
@@ -21,14 +26,25 @@ public class Main extends PApplet {
 
     @Override
     public void draw() {
+        background(255);
+
+        // Update W and H variables if needed
+        if (mousePressed && mouseButton == LEFT) {
+            W = Math.abs(mouseX - width / 2) * 2;
+            H = Math.abs(mouseY - height / 2) * 2;
+        }
+
+        // Draw the chassis
+        rectMode(CENTER);
+        strokeWeight(6);
+        strokeJoin(ROUND);
+        rect(width / 2, height / 2, W, H);
+
         // Values of the controller
         float[] values = input.getValues();
 
         float x1 = values[0];
         float y1 = values[1];
         float x2 = values[2];
-
-        // Temp for debug
-        System.out.println(x1 + " " + y1 + " " + x2);
     }
 }
