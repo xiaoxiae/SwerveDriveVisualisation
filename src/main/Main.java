0d54066bda0bb54913330f4db19c727cab452317
Unknown <tomas.slama.131@gmail.com>
2018-06-15 20:50:59 -0400

    add visualisation for motor vectors

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,9 @@ public class Main extends PApplet {
     private int W = 0;
     private int H = 0;
 
+    // The coordinates of the motors (calculated from W and H)
+    private float[][] motorCoordinates;
+
     static public void main(String[] passedArgs) {
         PApplet.main(Main.class.getName());
     }
@@ -34,6 +37,14 @@ public class Main extends PApplet {
         if (mousePressed && mouseButton == LEFT) {
             W = Math.abs(mouseX - width / 2) * 2;
             H = Math.abs(mouseY - height / 2) * 2;
+
+            // Update the coordinates of the motors
+            motorCoordinates = new float[][]{
+                    {width / 2 + W / 2, height / 2 - H / 2},
+                    {width / 2 - W / 2, height / 2 - H / 2},
+                    {width / 2 + W / 2, height / 2 + H / 2},
+                    {width / 2 - W / 2, height / 2 + H / 2}
+            };
         }
 
         // Draw the chassis of the robot
@@ -48,6 +59,66 @@ public class Main extends PApplet {
 
         // Draw the vector of the direction to which the robot is heading
         if (x1 != 0 || y1 != 0) drawVector(width / 2, height / 2, x1, y1, 50);
+
+        // The speed vectors of the motors
+        float[][] speedVectors = swerveCalculation(x1, y1, x2, W, H);
+
+        if (motorCoordinates != null) {
+            for (int i = 0; i < motorCoordinates.length; i++) {
+                drawVector(motorCoordinates[i][0], motorCoordinates[i][1], speedVectors[i][0], speedVectors[i][1], 30);
+            }
+        }
+    }
+
+    /**
+     * Calculates the speeds and the angles of the swerve chassis depending on the 3 axis inputs.
+     * <p>
+     * Mainly inspired by the brilliant Ether from Chief Delphi.
+     *
+     * @param x1 The left/right axis.
+     * @param y1 The forward/backward axis.
+     * @param x2 The rotation axis.
+     * @param W  Width of the chassis.
+     * @param H  Height of the chassis.
+     * @return A float[][], where each index is a vector of the speed and direction of the motor.
+     * @see <a href="https://www.chiefdelphi.com/media/papers/2426">Derivation of Inverse Kinematics for Swerve drive</a>
+     */
+    private float[][] swerveCalculation(float x1, float y1, float x2, float W, float H) {
+        // Change the values of W and H to be 1 at most
+        float normalizeWidthHeight = 1 / Math.max(W, H);
+        H *= normalizeWidthHeight;
+        W *= normalizeWidthHeight;
+
+        // Variables to calculate the speeds of the motors
+        float[] variables = new float[]{
+                x1 - x2 * H,
+                x1 + x2 * H,
+                y1 - x2 * W,
+                y1 + x2 * W
+        };
+
+        // Speeds of the motors
+        float[] speeds = new float[]{
+                (float) Math.sqrt(variables[1] * variables[1] + variables[3] * variables[3]),
+                (float) Math.sqrt(variables[1] * variables[1] + variables[2] * variables[2]),
+                (float) Math.sqrt(variables[0] * variables[0] + variables[3] * variables[3]),
+                (float) Math.sqrt(variables[0] * variables[0] + variables[2] * variables[2])
+        };
+
+        // Get the maximum speed of the motors
+        float maxMotorSpeed = speeds[0];
+        for (float speed : speeds) if (speed > maxMotorSpeed) maxMotorSpeed = speed;
+
+        // If the speed of the fastest motor is bigger than 1, normalize them all to under (or at) 1
+        if (maxMotorSpeed > 1) for (int i = 0; i < speeds.length; i++) variables[i] /= maxMotorSpeed;
+
+        // The x and y coordinates
+        return new float[][]{
+                {variables[1], variables[3]},
+                {variables[1], variables[2]},
+                {variables[0], variables[3]},
+                {variables[0], variables[2]}
+        };
     }
 
     /**
